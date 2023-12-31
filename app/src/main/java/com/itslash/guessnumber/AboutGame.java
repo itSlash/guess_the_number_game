eb4605e363b2a2452745348420cf5a3c4a14f49e
itSlash <102922084+itSlash@users.noreply.github.com>
2023-12-31 20:45:17 +0300

    Modify AboutGame.java

--- app/src/main/java/com/itslash/guessnumber/AboutGame.java
+++ app/src/main/java/com/itslash/guessnumber/AboutGame.java
@@ -17,7 +17,7 @@ public class AboutGame extends AppCompatActivity {
         Button button_back = findViewById(R.id.button_back);
         button_github.setOnClickListener(view -> {
             Intent intent_browse_link = new Intent(Intent.ACTION_VIEW);
-            intent_browse_link.setData(Uri.parse("https://github.com/"));
+            intent_browse_link.setData(Uri.parse("https://github.com/itSlash/guess_the_number_game"));
             startActivity(intent_browse_link);
         });
         button_back.setOnClickListener(view -> finish());
